b3f5a647226cd8c00f03d26e8132444b83f66dba
Arpan Rijal <arpanrijal.079@kathford.edu.np>
2026-03-24 15:21:53 +0545

    added final dataset

--- backend/controller/stockinfo.controller.js
+++ backend/controller/stockinfo.controller.js
@@ -1,7 +1,7 @@
 import { allStockInfo } from "../db/query/stockinfoQuery.js"
 const stockinfo = async (req, res) => {
     let stockinfodata = await allStockInfo()
-    console.log("stock info from route tab:", stockinfodata);
+    // console.log("stock info from route tab:", stockinfodata);
     
     res.status(200).json({status:true,message:stockinfodata})
 }
