925796032284380cf0245cdb31695f099d2237c1
Arpan Rijal <arpanrijal.079@kathford.edu.np>
2026-03-25 09:12:11 +0545

    added model and their performance with some backend changes

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise'
 import dotenv from 'dotenv/config'
 
-const db = await mysql.createConnection({
+const db = mysql.createPool({
     host: `${process.env.dbHost}`,
     user: `${process.env.dbUser_}`,
     password: `${process.env.dbUser_ps}`,
--- backend/db/db.predicted_price.js
+++ backend/db/db.predicted_price.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise'
 import dotenv from 'dotenv/config'
 
-const dbPP = await mysql.createConnection({
+const dbPP = mysql.createPool({
     host: `${process.env.dbHost}`,
     user: `${process.env.dbUser_}`,
     password: `${process.env.dbUser_ps}`,
